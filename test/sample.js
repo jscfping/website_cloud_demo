d6c1918259d7a68d75d80db02f0632b91e59b699
jscfping <jscfping@gmail.com>
2023-02-27 19:23:27 +0800

    test: add test sample

---
+++ test/sample.js
@@ -0,0 +1,25 @@
+
+const assert = require('assert');
+
+
+function add(a, b) {
+  return a + b;
+}
+
+
+
+// 測試 add 函式
+describe('add', function () {
+  I
+  it('adds 1 + 2 to equal 3', function () {
+    assert.strictEqual(add(1, 2), 3);
+  });
+
+  it('adds -1 + 5 to equal 4', function () {
+    assert.strictEqual(add(-1, 5), 4);
+  });
+
+  it('adds "hello" + "world" to equal "helloworld"', function () {
+    assert.strictEqual(add("hello", "world"), "helloworld");
+  });
+});
